4a5f74aa0bc9e7aae3450d102c5b62df40f535dc
NickMyers217 <nmyers@alitek.com>
2018-03-14 16:34:14 -0500

    Feat: add redux and begin implementing the redux store

--- src/App.js
+++ src/App.js
@@ -6,14 +6,20 @@ import Nav from './components/Nav';
 import InputData from './components/InputData';
 import TransformationPipeline from './components/TransformationPipeline';
 
+
+// TODO: refactoring and moving state into redux/redux-observable
+
+// TODO: scripting api (revisit after r&d into running???)
 // TODO: run (autorunning? except for ajax stuff/maybe mocking?)
     // TODO: passing data from node to node
     // TODO: accessing variables in a context???
+    // TODO: how to handle ajax requests (batching large amounts of data?)
     // TODO: UX and helpful messages?
+
 // TODO: debug feature?
 // TODO: autorun feature? (except for ajax operations)
 
-// TODO: cacheing, saving, and loading (redux with a local storage subscription???)
+// TODO: cacheing, saving, and loading (redux local storage subscription???) and a function that dumps/loads state from json
 
 class App extends Component {
     constructor(props) {
